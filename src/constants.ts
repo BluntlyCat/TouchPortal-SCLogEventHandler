2a5a087e69313ea5a0fcc1ceeb278872962c06ed
Mika Jünger <mika@bluntlycat.de>
2025-11-06 17:20:03 +0100

    Fix update URL

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const SC_ENVIRONMENT = 'Star Citizen Environment';
 export const SC_GAME_LOG_FILE = 'Star Citizen Game Log File';
 export const SC_READ_LOG_INTERVAL = 'Star Citizen Read Interval';
 export const SC_BLACKLIST = 'Star Citizen Blacklist';
-export const UPDATE_URL = 'https://raw.githubusercontent.com/BluntlyCat/TouchPortal-SCLogEventHandler/main/package.json';
+export const UPDATE_URL = 'https://raw.githubusercontent.com/BluntlyCat/TouchPortal-SCLogEventHandler/refs/tags/sc-log-event-handler-3.0.0/base/entry.tp';
 export const RELEASE_URL = 'https://raw.githubusercontent.com/BluntlyCat/TouchPortal-SCLogEventHandler/releases';
